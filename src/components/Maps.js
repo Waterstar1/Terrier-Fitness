90869db134536cf2a9b0cf2629fdb6d15fe7b964
Waterstar1 <waterstar12001@gmail.com>
2020-11-14 15:50:56 -0800

    New Repo

---
+++ src/components/Maps.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Maps.css';
+
+function Map() {
+  return (
+    <t> MAPS </t>
+  );
+}
+
+export default Map;
---
+++ src/components/pages/Services.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+import * as parkData from "./data/parks.json";
+import mapStyles from "./mapStyles";
+
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+
+  return (
+    <GoogleMap
+      defaultZoom={16}
+      defaultCenter={{ lat: 42.3504997, lng: -71.1075878 }}
+      defaultOptions={{ styles: mapStyles }}
+    >
+      {parkData.features.map(park => (
+        <Marker
+          key={park.properties.OBJECTID}
+          position={{
+            lat: park.geometry.coordinates[0][0][1],
+            lng: park.geometry.coordinates[0][0][0]
+          }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+          icon={{
+            url: `/kirby.jpg`,
+            scaledSize: new window.google.maps.Size(50, 50)
+          }}
+        />
+      ))}
+
+      {selectedPark && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0]
+          }}
+        >
+          <div>
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function Services() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyC1gXlilYT6zG937h44NvxdtP_ivFSKibY`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
