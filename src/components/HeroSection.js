d3e1c27f7cb07ae73aec1f6e8834344660f51522
Waterstar1 <waterstar12001@gmail.com>
2020-11-14 16:49:57 -0800

    Safely

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>EXERCISE SAFTELY</h1>
+      <h1>EXERCISE SAFELY</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
         <Button
