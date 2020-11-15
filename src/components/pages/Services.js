f7c66e8f1ea7b3c77a917162103b08499ea404bd
Waterstar1 <waterstar12001@gmail.com>
2020-11-14 16:09:51 -0800

    Fixed icons

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -43,8 +43,8 @@ function Map() {
             setSelectedPark(park);
           }}
           icon={{
-            url: `/kirby.jpg`,
-            scaledSize: new window.google.maps.Size(50, 50)
+            url: `/park-icon.png`,
+            scaledSize: new window.google.maps.Size(40, 40)
           }}
         />
       ))}
@@ -55,13 +55,12 @@ function Map() {
             setSelectedPark(null);
           }}
           position={{
-            lat: selectedPark.geometry.coordinates[1],
-            lng: selectedPark.geometry.coordinates[0]
+            lat: selectedPark.geometry.coordinates[0][0][1],
+            lng: selectedPark.geometry.coordinates[0][0][0]
           }}
         >
           <div>
-            <h2>{selectedPark.properties.NAME}</h2>
-            <p>{selectedPark.properties.DESCRIPTIO}</p>
+            <h2>{selectedPark.properties.SITE_NAME}</h2>
           </div>
         </InfoWindow>
       )}
