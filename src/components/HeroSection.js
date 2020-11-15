cf730b4cdaffb1a098542c6c27ec3e3fb10253e1
Waterstar1 <waterstar12001@gmail.com>
2020-11-14 16:44:10 -0800

    Fixed button links

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,6 +14,7 @@ function HeroSection() {
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
+          page='/services'
         >
           GET STARTED NOW
         </Button>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            Locations
+            Terrier Fitness
             <i class='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
@@ -61,7 +61,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+          {button && <Button buttonStyle='btn--outline' page='/sign-up'>SIGN UP</Button>}
         </div>
       </nav>
     </>
