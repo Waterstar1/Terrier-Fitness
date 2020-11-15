94c96692299d09ce9442b85005a59228c0cd6088
Shirley Yu <shirley_yu@berkeley.edu>
2020-11-14 16:47:46 -0800

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,8 +29,8 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            Locations
-            <i class='fab fa-typo3' />
+            <i class="fas fa-dog"></i>
+             Terrier Fitness
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
